304da5b6697184ec9b1bd2cc510dfa6e48d83302
Frank Osorio <frankosorio4@gmail.com>
2024-12-12 19:38:08 -0300

    feat: button component added

--- nearby/src/components/button/index.tsx
+++ nearby/src/components/button/index.tsx
@@ -1,47 +1,50 @@
 import {
-    TouchableOpacity, 
-    TouchableOpacityProps, 
-    Text, 
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Text,
     TextProps,
     ActivityIndicator
 } from "react-native";
-import {IconProps as TablerIconProps} from "@tabler/icons-react-native";
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native";
 
-import {s} from "./styles";
-import {colors} from "../../styles/theme";
+import { s } from "./styles";
+import { colors } from "../../styles/theme";
 
 type ButtonProps = TouchableOpacityProps & {
     isLoading?: boolean;
 }
 
 //we can use "style" as a prop given in the index in the app to pass another property
-function Button({children, style, isLoading = false}: ButtonProps) {
+function Button({ children, style, isLoading = false, ...rest }: ButtonProps) {
     return (
-        <TouchableOpacity style={[s.container, style]} activeOpacity={0.5} disabled={isLoading}>
+        <TouchableOpacity
+            style={[s.container, style]}
+            activeOpacity={0.5}
+            disabled={isLoading}
+            {...rest}
+        >
             {
                 isLoading ? (
-                <ActivityIndicator size="small" color={colors.gray[100]}/>) 
-                : children
+                    <ActivityIndicator size="small" color={colors.gray[100]} />)
+                    : children
             }
         </TouchableOpacity>
     );
 }
 
-function Title({children}: TextProps) {
-    return (
-        <Text style={s.title}>{children}</Text>
-    );
+function Title({ children }: TextProps) {
+    return <Text style={s.title}>{children}</Text>
 }
 
 type IconProps = {
     icon: React.ComponentType<TablerIconProps>
 }
 
-function Icon({icon: Icon}: IconProps) {
-    return <Icon size={24} color={colors.gray[100]}/>;
+function Icon({ icon: Icon }: IconProps) {
+    return <Icon size={24} color={colors.gray[100]} />;
 }
 
 Button.Title = Title
 Button.Icon = Icon
 
-export {Button};
+export { Button };
